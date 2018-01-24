aebeae9b99db5bf290f9d49565100ebcb6214e73
ighanavati <ighanavati@huskers.unl.edu>
2018-01-24 16:47:41 -0600

    Change #2
    Log10 should operate on the square

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -31,7 +31,8 @@ int isKaprekar(int n) {
 
   int i;
   long square = n * (long) n;
-  int numDigits = (int) log10(n) + 1;
+  //log should operate on square
+  int numDigits = (int) log10(square) + 1;
   long modulus = 0;
   long first, second;
   for(i=1; i<=numberOfDigits; i++) {
